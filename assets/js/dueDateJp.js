d751c425b003a4e32674b0a1b74b1ecdff2db7a9
Voxybuns <fossati.val@gmail.com>
2023-06-02 16:08:14 +0200

    fixed japanese date locales

--- assets/js/dueDateJp.js
+++ assets/js/dueDateJp.js
@@ -2,8 +2,8 @@
 var dateStart = Date.UTC(2023, 5, 3, 22, 59, 0);
 var dateEnd = Date.UTC(2023, 5, 10, 22, 59, 0);
 
-var startDate = new Intl.DateTimeFormat(['en-US', 'en-GB'], { dateStyle: 'medium', timeStyle: 'short'}).format(dateStart);
-var dueDate = new Intl.DateTimeFormat(['en-US', 'en-GB'], { dateStyle: 'medium', timeStyle: 'short'}).format(dateEnd);
+var startDate = new Intl.DateTimeFormat('ja-JP', { dateStyle: 'medium', timeStyle: 'short'}).format(dateStart);
+var dueDate = new Intl.DateTimeFormat('ja-JP', { dateStyle: 'medium', timeStyle: 'short'}).format(dateEnd);
 
 var startDateEl = document.getElementById("startDate");
 startDateEl.textContent = startDate;
